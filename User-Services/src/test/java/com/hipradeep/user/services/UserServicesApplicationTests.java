85d7448fc4d396c9e3616f002aba1661e9a83bd4
hipradeep <pradeepmaurya.cse@gmail.com>
2025-08-09 11:35:23 +0530

    update

--- User-Services/src/test/java/com/hipradeep/user/services/UserServicesApplicationTests.java
+++ User-Services/src/test/java/com/hipradeep/user/services/UserServicesApplicationTests.java
@@ -1,7 +1,6 @@
 package com.hipradeep.user.services;
 
 import com.hipradeep.user.services.entities.Rating;
-import com.hipradeep.user.services.external.services.RatingService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,20 +9,5 @@ import org.springframework.http.ResponseEntity;
 @SpringBootTest
 class UserServicesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
-
-	@Autowired
-	private RatingService ratingService;
-
-	@Test
-	void createRating() {
-		Rating rating = Rating.builder().rating(10)
-				.userId("").hotelId("").feedback("this is created using feign client").build();
-		ResponseEntity<Rating> ratingResponseEntity = ratingService.createRating(rating);
-		System.out.println("new rating created");
-	}
 
 }
